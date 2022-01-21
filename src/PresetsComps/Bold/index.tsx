7151b997ffcfc92edad69ba789565988833026c7
clydee <641365724@qq.com>
2022-01-21 14:30:11 +0800

    active

--- src/PresetsComps/Bold/index.tsx
+++ src/PresetsComps/Bold/index.tsx
@@ -10,13 +10,31 @@ interface IProps {
 
 const Bold: React.FC<IProps> = (props) => {
   const { editorState, setEditorState } = props;
+
+  /**
+   * methods
+   */
+
+  const isActiveBindFn = () => {
+    let isActive = false;
+    const currentStyle = editorState.getCurrentInlineStyle();
+    if (currentStyle.has("BOLD")) {
+      isActive = true;
+    }
+    return isActive;
+  };
+
+  /**
+   * jsx
+   */
   return (
     <ButtonLayout
       icon={<BoldOutlined />}
+      isActive={isActiveBindFn()}
       tip="加粗"
-      clickPropsFn={() =>
-        setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"))
-      }
+      clickPropsFn={() => {
+        setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
+      }}
     />
   );
 };
--- src/components/ButtonLayout/index.tsx
+++ src/components/ButtonLayout/index.tsx
@@ -1,14 +1,16 @@
 import { Button, Tooltip } from "antd";
 import { ReactNode } from "react";
-
+import styles from "./index.less";
+import classnames from "classnames";
 interface IProps {
   icon: ReactNode;
   tip?: ReactNode;
   clickPropsFn?: () => void;
+  isActive?: boolean;
 }
 
 const ButtonLayout: React.FC<IProps> = (props) => {
-  const { icon, tip, clickPropsFn } = props;
+  const { icon, tip, clickPropsFn, isActive } = props;
 
   /**
    * jsx
@@ -16,7 +18,16 @@ const ButtonLayout: React.FC<IProps> = (props) => {
 
   return (
     <Tooltip title={tip}>
-      <Button type="text" icon={icon} onClick={clickPropsFn}></Button>
+      <Button
+        type="text"
+        icon={icon}
+        onClick={clickPropsFn}
+        className={
+          isActive
+            ? classnames(styles.active, styles.ButtonLayout)
+            : styles.ButtonLayout
+        }
+      ></Button>
     </Tooltip>
   );
 };
