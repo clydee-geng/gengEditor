620ab7d6468541416c106b26cae6770d78a4bf75
clydee <641365724@qq.com>
2022-04-17 19:54:08 +0800

    feat: 文档页面打包

--- dumi/.umirc.ts
+++ dumi/.umirc.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  outputPath: 'docs-dist',
-  // more config: https://d.umijs.org/config
+  description: "基于antd和draft-js的React富文本编辑器",
+  outputPath: '../docs-dist',
 });
