195559f0014d506386504edde6afadeb5910da1d
clydee <641365724@qq.com>
2022-04-17 20:15:02 +0800

    feat: 修改base

--- dumi/.umirc.ts
+++ dumi/.umirc.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   description: "基于antd和draft-js的React富文本编辑器",
   outputPath: '../docs', // 需要设置github的 page source
-  publicPath: '/gengEditor/'
+  publicPath: '/gengEditor/',
+  base: '/gengEditor/',
 });
