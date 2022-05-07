af2bd06745768c523a0cd5ba9c7e8dedfd7796f6
clydee <641365724@qq.com>
2022-05-07 19:59:18 +0800

    fix: 解决Unknown DraftEntity key: null问题

--- src/PresetsComps/Media/Content/index.tsx
+++ src/PresetsComps/Media/Content/index.tsx
@@ -7,33 +7,33 @@ import styles from "./index.less";
 import classnames from "classnames";
 
 interface IProps {
-  block: ContentBlock;
-  contentState: ContentState;
-  blockProps: {
-    editorContentDom: HTMLElement;
-    editorState: EditorState;
-    setEditorState: any;
-    curSelectBlock?: ContentBlock;
-    disabled: boolean;
-  };
+	block: ContentBlock;
+	contentState: ContentState;
+	blockProps: {
+		editorContentDom: HTMLElement;
+		editorState: EditorState;
+		setEditorState: any;
+		curSelectBlock?: ContentBlock;
+		disabled: boolean;
+	};
 }
 const Media: React.FC<IProps> = (props) => {
-  const { block, contentState, blockProps } = props;
-  const { disabled } = blockProps;
-  const entitykey = block.getEntityAt(0);
-  const type = contentState.getEntity(entitykey).getType();
+	const { block, contentState, blockProps } = props;
+	const { disabled } = blockProps;
+	const entitykey = block.getEntityAt(0);
+	const type = entitykey && contentState.getEntity(entitykey).getType();
 
-  return (
-    <div
-      className={
-        disabled ? classnames(styles.Media, styles.MediaDisable) : styles.Media
-      }
-    >
-      {type === "IMAGE" && <ImageContent {...props} />}
-      {type === "VIDEO" && <VideoContent {...props} />}
-      {type === "AUDIO" && <AudioContent {...props} />}
-    </div>
-  );
+	return (
+		<div
+			className={
+				disabled ? classnames(styles.Media, styles.MediaDisable) : styles.Media
+			}
+		>
+			{type === "IMAGE" && <ImageContent {...props} />}
+			{type === "VIDEO" && <VideoContent {...props} />}
+			{type === "AUDIO" && <AudioContent {...props} />}
+		</div>
+	);
 };
 
 export default Media;
