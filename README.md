fd9505bacc25d90751d911e10efde825056c3ee4
clydee <641365724@qq.com>
2022-05-07 14:11:43 +0800

    feat: 更新文档

--- README.md
+++ README.md
@@ -8,11 +8,12 @@
 
 ## ✨ Feature
 
-- 🛡 使用 React hooks 和 Typescript 编写；
-- 👬 和 antd 无缝结合，上手简单；
-- 💪 可作为受控组件，用于表单选项；
+- 🔥 使用热门的 React hooks 和 Typescript 编写，紧跟时代潮流；
+- 👬 开箱即用，和 antd 无缝结合；并且可作为受控组件，用于表单选项；
 - 💪 基于和 React 师出同门的 draft.js 编写，和 React 无缝结合；
+- 🎨 集成 color-pickr，轻松添加更多颜色
+- ⬆️ 自定义媒体文件上传方法，上传文件更加灵活
 
 ## use
 
-查看文档 👉  https://clydee-geng.github.io/gengEditor/
+查看文档 👉 https://clydee-geng.github.io/gengEditor/
--- example/index.js
+++ example/index.js
@@ -50,9 +50,7 @@ const App = () => {
 
 	const defaultValue =
 		'<p><strong>1111111</strong></p><p style="text-align:center;"><em><span style="font-size:48px">2222222</span></em></p><p><span style="font-size:24px">3333333</span></p><p><u>44444</u></p><p><span style="text-decoration:line-through">55555</span></p><p><span style="color:#FF0000">66666</span></p><p><span style="background-color:#FFA500">77777</span></p><p><a href="88888">88888</a></p><ul><li>9999</li></ul><ol><li style="text-align:center;">101010</li></ol><h1 style="text-align:center;">11111111</h1><blockquote style="text-align:center;">121211212<br/>1211212<br/>12211</blockquote><pre>13131331313<br/>   13131313<br/></pre><span style="line-height: 3";>1414314414</span><p>15151515</p><p>16161616</p><p></p><p></p><figure style="text-align:center;"><img src="https://s2.ax1x.com/2020/02/29/3yhm8S.jpg" style="width:166px;height:237px;" /></figure><p></p><figure><video src="https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4" style="width:100%;" controls /></figure><p></p><figure><audio src="https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4" style="width:100%;" controls /></figure><p></p><p></p><p></p>';
-	const [value, setValue] = React.useState(
-		'<p></p><figure style="text-align:center;"><img src="https://s2.ax1x.com/2020/02/29/3yhm8S.jpg" style="width:175px;height:250.25px;" /></figure><p></p>'
-	);
+	const [value, setValue] = React.useState(defaultValue);
 
 	return (
 		<div style={{ width: 800 }}>
