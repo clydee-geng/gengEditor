442a70fcf2b2b077524ba586827e6f11994ca2bc
clydee <641365724@qq.com>
2022-05-18 23:57:33 +0800

    feat: ts编译配置

--- src/alias/components/ButtonLayout/index.tsx
+++ src/alias/components/ButtonLayout/index.tsx
@@ -2,50 +2,51 @@ import { Button, Tooltip } from "antd";
 import { ReactNode } from "react";
 import styles from "./index.less";
 import classnames from "classnames";
+import React from "react";
 interface IProps {
-  icon: ReactNode;
-  tip?: ReactNode;
-  clickPropsFn?: () => void;
-  activeColor?: string | boolean;
-  activeColorCSSProp?: "color" | "backgroundColor";
-  disabled?: boolean;
+	icon: ReactNode;
+	tip?: ReactNode;
+	clickPropsFn?: () => void;
+	activeColor?: string | boolean;
+	activeColorCSSProp?: "color" | "backgroundColor";
+	disabled?: boolean;
 }
 
 const ButtonLayout: React.FC<IProps> = (props) => {
-  const {
-    icon,
-    tip,
-    clickPropsFn,
-    activeColor,
-    activeColorCSSProp = "color",
-    disabled = false,
-  } = props;
+	const {
+		icon,
+		tip,
+		clickPropsFn,
+		activeColor,
+		activeColorCSSProp = "color",
+		disabled = false,
+	} = props;
 
-  /**
-   * jsx
-   */
-  return (
-    <Tooltip title={tip}>
-      <Button
-        disabled={disabled}
-        type="text"
-        icon={icon}
-        onClick={clickPropsFn}
-        style={
-          typeof activeColor === "string"
-            ? { [activeColorCSSProp]: activeColor }
-            : {}
-        }
-        className={
-          activeColor
-            ? activeColorCSSProp === "color"
-              ? classnames(styles.activeForColor, styles.ButtonLayout)
-              : classnames(styles.activeForBgColor, styles.ButtonLayout)
-            : styles.ButtonLayout
-        }
-      ></Button>
-    </Tooltip>
-  );
+	/**
+	 * jsx
+	 */
+	return (
+		<Tooltip title={tip}>
+			<Button
+				disabled={disabled}
+				type="text"
+				icon={icon}
+				onClick={clickPropsFn}
+				style={
+					typeof activeColor === "string"
+						? { [activeColorCSSProp]: activeColor }
+						: {}
+				}
+				className={
+					activeColor
+						? activeColorCSSProp === "color"
+							? classnames(styles.activeForColor, styles.ButtonLayout)
+							: classnames(styles.activeForBgColor, styles.ButtonLayout)
+						: styles.ButtonLayout
+				}
+			></Button>
+		</Tooltip>
+	);
 };
 
 export default ButtonLayout;
